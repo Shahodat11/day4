dac83eefd78115f6d65873e283e99f72a08ce18d
unknown <shahodatusmonjonova@gmail.com>
2024-04-13 01:05:22 +0500

    davom

---
+++ src/components/daverya/daverya.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function daverya() {
+  return (
+    <div>daverya</div>
+  )
+}
+
+export default daverya
--- src/components/footerTop/FooterTop.js
+++ src/components/footerTop/FooterTop.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import '../footerTop/footerTop.css'
 import image from '../images/image 1.svg'
+import main from '../images/clients logo.svg'
+import mainLogo from '../images/our team.svg'
 
 function FooterTop() {
   return (
     <>
+    <div className="container">
+      <img src={main} alt="" />
+      <img src={mainLogo} alt="" />
     <div className="nav-link__img">
         <img src={image} alt="" />
+    </div>        
     </div>
+
     </>
   )
 }
